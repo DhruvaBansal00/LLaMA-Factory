458c1f51c0f1f481f6504cd75757d143daab471f
DhruvaBansal00 <dhruva.bansal00@gmail.com>
2025-01-16 15:44:30 -0800

    update reqs

--- setup.py
+++ setup.py
@@ -50,11 +50,11 @@ def get_console_scripts() -> List[str]:
 
 
 extra_require = {
-    "torch": ["torch>=1.13.1"],
+    "torch": ["torch>=2.3.1"],
     "torch-npu": ["torch==2.1.0", "torch-npu==2.1.0.post3", "decorator"],
     "metrics": ["nltk", "jieba", "rouge-chinese"],
     "deepspeed": ["deepspeed>=0.10.0,<=0.14.4"],
-    "liger-kernel": ["liger-kernel"],
+    "liger-kernel": ["liger-kernel==0.4.2"],
     "bitsandbytes": ["bitsandbytes>=0.39.0"],
     "hqq": ["hqq"],
     "eetq": ["eetq"],
