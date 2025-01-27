353902fa0efd905e3925d696a6cb3ee3c5900c42
DhruvaBansal00 <dhruva.bansal00@gmail.com>
2025-01-27 15:53:28 -0800

    Backing up models in aws directly

--- src/llamafactory/train/sft/trainer.py
+++ src/llamafactory/train/sft/trainer.py
@@ -20,6 +20,7 @@ import os
 from types import MethodType
 from typing import TYPE_CHECKING, Any, Dict, List, Optional, Tuple, Union
 
+import boto3
 import numpy as np
 import torch
 from transformers import Seq2SeqTrainer
@@ -49,12 +50,15 @@ class CustomSeq2SeqTrainer(Seq2SeqTrainer):
     """
 
     def __init__(
-        self, finetuning_args: "FinetuningArguments", processor: Optional["ProcessorMixin"], **kwargs
+        self,
+        finetuning_args: "FinetuningArguments",
+        processor: Optional["ProcessorMixin"],
+        **kwargs,
     ) -> None:
         if is_transformers_version_greater_than("4.46"):
             kwargs["processing_class"] = kwargs.pop("tokenizer")
         else:
-            self.processing_class: "PreTrainedTokenizer" = kwargs.get("tokenizer")
+            self.processing_class: PreTrainedTokenizer = kwargs.get("tokenizer")
 
         super().__init__(**kwargs)
         self.finetuning_args = finetuning_args
@@ -65,18 +69,28 @@ class CustomSeq2SeqTrainer(Seq2SeqTrainer):
         if finetuning_args.use_badam:
             from badam import BAdamCallback, clip_grad_norm_old_version  # type: ignore
 
-            self.accelerator.clip_grad_norm_ = MethodType(clip_grad_norm_old_version, self.accelerator)
+            self.accelerator.clip_grad_norm_ = MethodType(
+                clip_grad_norm_old_version,
+                self.accelerator,
+            )
             self.add_callback(BAdamCallback)
+        self.s3_client = boto3.client("s3")
 
     @override
     def create_optimizer(self) -> "torch.optim.Optimizer":
         if self.optimizer is None:
-            self.optimizer = create_custom_optimizer(self.model, self.args, self.finetuning_args)
+            self.optimizer = create_custom_optimizer(
+                self.model,
+                self.args,
+                self.finetuning_args,
+            )
         return super().create_optimizer()
 
     @override
     def create_scheduler(
-        self, num_training_steps: int, optimizer: Optional["torch.optim.Optimizer"] = None
+        self,
+        num_training_steps: int,
+        optimizer: Optional["torch.optim.Optimizer"] = None,
     ) -> "torch.optim.lr_scheduler.LRScheduler":
         create_custom_scheduler(self.args, num_training_steps, optimizer)
         return super().create_scheduler(num_training_steps, optimizer)
@@ -90,7 +104,11 @@ class CustomSeq2SeqTrainer(Seq2SeqTrainer):
 
     @override
     def compute_loss(
-        self, model: "PreTrainedModel", inputs: Dict[str, "torch.Tensor"], return_outputs: bool = False, **kwargs
+        self,
+        model: "PreTrainedModel",
+        inputs: Dict[str, "torch.Tensor"],
+        return_outputs: bool = False,
+        **kwargs,
     ) -> Union["torch.Tensor", Tuple["torch.Tensor", List["torch.Tensor"]]]:
         r"""
         Fixes the loss value. See https://github.com/huggingface/transformers/pull/35438 for details.
@@ -98,7 +116,11 @@ class CustomSeq2SeqTrainer(Seq2SeqTrainer):
         It should be removed after https://github.com/huggingface/transformers/pull/35651 is merged.
         """
         loss = super().compute_loss(model, inputs, return_outputs, **kwargs)
-        if kwargs.get("num_items_in_batch") and not getattr(self, "model_accepts_loss_kwargs", False):
+        if kwargs.get("num_items_in_batch") and not getattr(
+            self,
+            "model_accepts_loss_kwargs",
+            False,
+        ):
             if return_outputs:
                 loss = (loss[0] / self.args.gradient_accumulation_steps, *loss[1:])
             else:
@@ -126,16 +148,25 @@ class CustomSeq2SeqTrainer(Seq2SeqTrainer):
             labels = inputs.get("labels")
 
         loss, generated_tokens, _ = super().prediction_step(
-            model, inputs, prediction_loss_only=prediction_loss_only, ignore_keys=ignore_keys, **gen_kwargs
+            model,
+            inputs,
+            prediction_loss_only=prediction_loss_only,
+            ignore_keys=ignore_keys,
+            **gen_kwargs,
         )
         if generated_tokens is not None and self.args.predict_with_generate:
-            generated_tokens[:, : inputs["input_ids"].size(-1)] = self.processing_class.pad_token_id
+            generated_tokens[:, : inputs["input_ids"].size(-1)] = (
+                self.processing_class.pad_token_id
+            )
             generated_tokens = generated_tokens.contiguous()
 
         return loss, generated_tokens, labels
 
     def save_predictions(
-        self, dataset: "Dataset", predict_results: "PredictionOutput", skip_special_tokens: bool = True
+        self,
+        dataset: "Dataset",
+        predict_results: "PredictionOutput",
+        skip_special_tokens: bool = True,
     ) -> None:
         r"""
         Saves model predictions to `output_dir`.
@@ -145,11 +176,16 @@ class CustomSeq2SeqTrainer(Seq2SeqTrainer):
         if not self.is_world_process_zero():
             return
 
-        output_prediction_file = os.path.join(self.args.output_dir, "generated_predictions.jsonl")
+        output_prediction_file = os.path.join(
+            self.args.output_dir,
+            "generated_predictions.jsonl",
+        )
         logger.info_rank0(f"Saving prediction results to {output_prediction_file}")
 
         labels = np.where(
-            predict_results.label_ids != IGNORE_INDEX, predict_results.label_ids, self.processing_class.pad_token_id
+            predict_results.label_ids != IGNORE_INDEX,
+            predict_results.label_ids,
+            self.processing_class.pad_token_id,
         )
         preds = np.where(
             predict_results.predictions != IGNORE_INDEX,
@@ -160,12 +196,94 @@ class CustomSeq2SeqTrainer(Seq2SeqTrainer):
         for i in range(len(preds)):
             pad_len = np.nonzero(preds[i] != self.processing_class.pad_token_id)[0]
             if len(pad_len):  # move pad token to last
-                preds[i] = np.concatenate((preds[i][pad_len[0] :], preds[i][: pad_len[0]]), axis=-1)
+                preds[i] = np.concatenate(
+                    (preds[i][pad_len[0] :], preds[i][: pad_len[0]]),
+                    axis=-1,
+                )
 
-        decoded_inputs = self.processing_class.batch_decode(dataset["input_ids"], skip_special_tokens=False)
-        decoded_preds = self.processing_class.batch_decode(preds, skip_special_tokens=skip_special_tokens)
-        decoded_labels = self.processing_class.batch_decode(labels, skip_special_tokens=skip_special_tokens)
+        decoded_inputs = self.processing_class.batch_decode(
+            dataset["input_ids"],
+            skip_special_tokens=False,
+        )
+        decoded_preds = self.processing_class.batch_decode(
+            preds,
+            skip_special_tokens=skip_special_tokens,
+        )
+        decoded_labels = self.processing_class.batch_decode(
+            labels,
+            skip_special_tokens=skip_special_tokens,
+        )
 
         with open(output_prediction_file, "w", encoding="utf-8") as f:
             for text, pred, label in zip(decoded_inputs, decoded_preds, decoded_labels):
-                f.write(json.dumps({"prompt": text, "predict": pred, "label": label}, ensure_ascii=False) + "\n")
+                f.write(
+                    json.dumps(
+                        {"prompt": text, "predict": pred, "label": label},
+                        ensure_ascii=False,
+                    )
+                    + "\n",
+                )
+
+    def _save_checkpoint(self, model, trial, metrics=None):
+        # In all cases, including ddp/dp/deepspeed, self.model is always a reference to the model we
+        # want to save except FullyShardedDDP.
+        # assert unwrap_model(model) is self.model, "internal model should be a reference to self.model"
+
+        # Save model checkpoint
+        checkpoint_folder = f"checkpoint-{self.state.global_step}"
+
+        if self.hp_search_backend is None and trial is None:
+            self.store_flos()
+
+        run_dir = self._get_output_dir(trial=trial)
+        output_dir = os.path.join(run_dir, checkpoint_folder)
+        self.save_model(output_dir, _internal_call=True)
+
+        if not self.args.save_only_model:
+            # Save optimizer and scheduler
+            self._save_optimizer_and_scheduler(output_dir)
+            # Save RNG state
+            self._save_rng_state(output_dir)
+
+        s3_bucket_name = "refuel-llm-v3"
+        for root, dirs, files in os.walk(output_dir):
+            for file in files:
+                local_path = os.path.join(root, file)
+                relative_path = os.path.relpath(local_path, output_dir)
+                s3_path = os.path.join(os.path.basename(output_dir), relative_path)
+                # Upload file to S3
+                self.s3_client.upload_file(
+                    local_path,
+                    s3_bucket_name,
+                    s3_path,
+                )
+                print(f"Uploaded {local_path} to {s3_path} in {s3_bucket_name}")
+
+        # Determine the new best metric / best model checkpoint
+        if metrics is not None and self.args.metric_for_best_model is not None:
+            metric_to_check = self.args.metric_for_best_model
+            if not metric_to_check.startswith("eval_"):
+                metric_to_check = f"eval_{metric_to_check}"
+            metric_value = metrics[metric_to_check]
+
+            operator = np.greater if self.args.greater_is_better else np.less
+            if (
+                self.state.best_metric is None
+                or self.state.best_model_checkpoint is None
+                or operator(metric_value, self.state.best_metric)
+            ):
+                self.state.best_metric = metric_value
+                self.state.best_model_checkpoint = output_dir
+
+        # Save the Trainer state
+        if self.args.should_save:
+            self.state.save_to_json(os.path.join(output_dir, "trainer_state.json"))
+
+        if self.args.push_to_hub:
+            self._push_from_checkpoint(output_dir)
+
+        # Maybe delete some older checkpoints.
+        if self.args.should_save:
+            # Solely rely on numerical checkpoint id for rotation.
+            # mtime is not reliable especially on some fuse fs in cloud environments.
+            self._rotate_checkpoints(use_mtime=False, output_dir=run_dir)
